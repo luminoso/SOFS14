ecfefa6cc169e31f5bf041535656f6bd0999cdbe
gjc@ua.pt <gjc@ua.pt@d350dffd-24a3-4275-a452-e23bb94b2ded>
2014-09-30 16:04:11 +0000

    funcao soFreeDataCluster() completa e pronta aparentemente passa em todos os vectores de teste do professor

--- src/sofs14/sofs_ifuncs_1/soFreeDataCluster.c
+++ src/sofs14/sofs_ifuncs_1/soFreeDataCluster.c
@@ -1,7 +1,7 @@
 /**
  *  \file soFreeDataCluster.c (implementation file)
  *
- *  \author
+ *  \author Guilherme Cardoso - 45726
  */
 
 #include <stdio.h>
@@ -50,9 +50,46 @@ int soDeplete (SOSuperBlock *p_sb);
 int soFreeDataCluster (uint32_t nClust)
 {
   soColorProbe (614, "07;33", "soFreeDataCluster (%"PRIu32")\n", nClust);
-
-  /* insert your code here */
-
+  
+  int stat;
+  SOSuperBlock *p_sb;
+  uint32_t  NFClt;
+  SODataClust datacluster;
+  
+  if ((stat = soLoadSuperBlock ()) != 0)
+    return stat;
+  p_sb = soGetSuperBlock();
+  
+  // validação EINVAL
+  if(nClust > p_sb->dZoneTotal) return  -EINVAL;
+  if(nClust <= 0) return -EINVAL;
+  
+  NFClt = p_sb->dZoneStart + nClust * BLOCKS_PER_CLUSTER;
+  
+  if((stat = soReadCacheCluster(NFClt,&datacluster)) != 0)
+    return stat;
+  
+  if(datacluster.stat == NULL_INODE) return -EDCNALINVAL;
+  
+  uint32_t p_stat;
+  if((stat = soQCheckStatDC(p_sb,nClust,&p_stat)) != 0)
+    return stat;
+  
+  if(p_stat != ALLOC_CLT) return -EDCNALINVAL;
+  
+  datacluster.prev = datacluster.next = NULL_CLUSTER;
+  
+  // verificar se a cache de insercao está cheia
+  if(p_sb->dZoneInsert.cacheIdx == DZONE_CACHE_SIZE)
+    soDeplete(p_sb);
+  
+  p_sb->dZoneInsert.cache[p_sb->dZoneInsert.cacheIdx] = nClust;
+  p_sb->dZoneInsert.cacheIdx +=1;
+  p_sb->dZoneFree += 1;
+  
+  if((stat = soWriteCacheCluster(NFClt,&datacluster)) != 0)
+    return stat;
+  
   return 0;
 }
 
@@ -70,8 +107,66 @@ int soFreeDataCluster (uint32_t nClust)
 
 int soDeplete (SOSuperBlock *p_sb)
 {
-
-  /* insert your code here */
-
+  int stat;                                             // variavel para controlar o retorno das funcoes chamadas
+  SODataClust datacluster;
+  uint32_t NFClt;
+  
+  if(p_sb->dTail != NULL_CLUSTER){
+    NFClt =  p_sb->dZoneStart + p_sb->dTail*BLOCKS_PER_CLUSTER;
+    if((stat = soReadCacheCluster(NFClt,&datacluster)) != 0)
+      return stat;
+    datacluster.next = p_sb->dZoneInsert.cache[0];
+    
+    if((stat = soWriteCacheCluster(p_sb->dTail,&datacluster)) !=0)
+      return stat;
+  }
+  
+  int k;
+  for(k = 0; k < p_sb->dZoneInsert.cacheIdx; k++){
+    if(k == 0){
+      NFClt =  p_sb->dZoneStart + p_sb->dZoneInsert.cache[0]*BLOCKS_PER_CLUSTER;
+      if((stat = soReadCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+      datacluster.prev = p_sb->dZoneInsert.cache[k-1];
+      if((stat = soWriteCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+    } else {
+      NFClt = p_sb->dZoneStart + p_sb->dZoneInsert.cache[k] * BLOCKS_PER_CLUSTER;
+      if((stat = soReadCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+      datacluster.prev = p_sb->dZoneInsert.cache[k-1];
+      if((stat = soWriteCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+    }
+    if(k != (p_sb->dZoneInsert.cacheIdx -1)){
+      NFClt =  p_sb->dZoneStart + p_sb->dZoneInsert.cache[k]*BLOCKS_PER_CLUSTER;
+      if((stat = soReadCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+      datacluster.next = p_sb->dZoneInsert.cache[k+1];
+      if((stat = soWriteCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+    } else {
+      NFClt =  p_sb->dZoneStart + p_sb->dZoneInsert.cache[k]*BLOCKS_PER_CLUSTER;
+      if((stat = soReadCacheCluster(NFClt,&datacluster)) != 0)
+        return stat;
+      datacluster.next = NULL_CLUSTER;
+    }
+  }
+  
+  p_sb->dTail = p_sb->dZoneInsert.cache[p_sb->dZoneInsert.cacheIdx - 1];
+  
+  if(p_sb->dHead == NULL_CLUSTER)
+    p_sb->dHead = p_sb->dZoneInsert.cache[0];
+  
+  for(k = 0; k < p_sb->dZoneInsert.cacheIdx; k++)
+    p_sb->dZoneInsert.cache[k] = NULL_CLUSTER;
+  
+  p_sb->dZoneInsert.cacheIdx = 0;
+  
+  // gravar as alteracoes feitas no super bloco
+  if((stat = soStoreSuperBlock()) != 0)
+    return stat;
+  
+  // se tudo OK retornar 0
   return 0;
 }
