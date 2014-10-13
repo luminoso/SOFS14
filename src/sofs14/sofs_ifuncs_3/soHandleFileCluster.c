35ee7b96ae9eaad671e03ed2148927471647dc03
gjc <gjc@ua.pt>
2014-10-13 19:55:52 +0000

    soHandleFileCluster() -corrigida a chamada das funcões mais especificas. Não é com switch-case, mas sim pelo range do clustInd

--- src/sofs14/sofs_ifuncs_3/soHandleFileCluster.c
+++ src/sofs14/sofs_ifuncs_3/soHandleFileCluster.c
@@ -110,62 +110,15 @@ int soHandleFileCluster(uint32_t nInode, uint32_t clustInd, uint32_t op, uint32_
 
     // if op=GET|ALLOC|FREE|FREE_CLEAN check if nInode is in use and its type is valid
     // if op=CLEAN check quick free dirty state
+    
+    SOInode *p_inode;
 
-    /* calculate if clustInd is direct, single indirect or double indirect table
-     * 0 - direct reference
-     * 1 - single indirect reference
-     * 3 - double indirect reference
-     */
-    unsigned int rt; // reference table indicator. 0, 1 or 2
-    unsigned int rp; // reference position for a given table
-    unsigned float rp_offset; // offset position for the given table
-    
-    rp = (clustInd - N_DIRECT) / RPC;
-    rp_offset = (clustInd - N_DIRECT) % RPC;
-    
-    if(clustInd < 7){ // if smaller than 7 it's located at the direct reference table
-        rt = 0; // requested cluster id is located in direct reference table
-    } else if(rp <= 1 && rp_offset == 0){ // if it fit in single indirect
-        rt = 1; // requested cluster id is located in single indirect table
-    } else if(rp >= 1 && rp_offset != 0){ // if it fit in the double indirect table
-        rt = 2; // requested cluster id is located double indirect reference table
-    }
-    
-    switch (op) {
-        case GET:{
-            switch(rt){
-                case 0:{} // soHandleDirect
-                case 1:{} // soHandleSIndirect
-                case 2:{} // soHandleDIndirect
-                default:{} // ??
-            }
-        }
-        case ALLOC:{
-        switch(rt){
-                case 0:{} // soHandleDirect
-                case 1:{}
-                case 2:{}
-                default:{}
-            }
-        }
-        case FREE:{switch(rt){
-                case 0:{}
-                case 1:{}
-                case 2:{}
-                default:{}
-            }
-        }
-        case FREE_CLEAN:{
-        switch(rt){
-                case 0:{}
-                case 1:{}
-                case 2:{}
-                default:{}
-            }
-        }
-        case CLEAN:{}
-        default:
-            return -EINVAL;
+    if(clustInd <= N_DIRECT){
+        soHandleDirect(p_sb,nInode,p_inode,clustInd,op,p_outVal);
+    } else if (clustInd <= N_DIRECT + RPC) {
+        soHandleSIndirect(p_sb,nInode,p_inode,clustInd,op,p_outVal);
+    } else {
+        soHandleDIndirect(p_sb,nInode,p_inode,clustInd,op,p_outVal);
     }
 
     return 0;
