3fdae1e1800298640cdfbc9dbdb8641b67fe7ff8
joao.cravo <joao.cravo@ua.pt>
2014-10-27 09:16:59 +0000

    soGetDirEntryByName

--- src/sofs14/sofs_ifuncs_4/soGetDirEntryByName.c
+++ src/sofs14/sofs_ifuncs_4/soGetDirEntryByName.c
@@ -1,7 +1,7 @@
 /**
  *  \file soGetDirEntryByName.c (implementation file)
  *
- *  \author
+ *  \author João Cravo, 63784
  */
 
 #include <stdio.h>
@@ -64,7 +64,102 @@ int soGetDirEntryByName (uint32_t nInodeDir, const char *eName, uint32_t *p_nIno
   soColorProbe (312, "07;31", "soGetDirEntryByName (%"PRIu32", \"%s\", %p, %p)\n",
                 nInodeDir, eName, p_nInodeEnt, p_idx);
 
-  /* insert your code here */
+  int stat;
+  SOSuperBlock *p_sb;
+  SOInode inode;
+  SODataClust dc;
+  int i = 0;
+  
+  //Carregar e obter SUper Bloco
+  if((stat = soLoadSuperBlock()) != 0)
+      return stat;
+  
+  p_sb = soGetSuperBlock();
+  
+  //Verificar parametros do nInodeDir
+  if(nInodeDir < 0 || nInodeDir > (p_sb->iTotal - 1))
+  	return -EINVAL;
+  
+  //Verificar eName
+  if(eName == NULL)
+  	return -EINVAL;
+       
+  //Validação da String 
+  while(eName[i] != '\0'){
 
-  return 0;
+    //  eName não pode conter '/' porque senão seria um caminho 
+    if(eName[i] == '/'){
+      return -EINVAL;
+    }
+    i++;
+  }
+  
+  //Verificar comprimento do nome
+  if(i > MAX_NAME)
+    return -ENAMETOOLONG;
+  
+  //Ler o inode desejado
+  if((stat = soReadInode(&inode, nInodeDir, IUIN)) != 0)
+    return stat;
+  
+  //Verificar se o inode é um directorio
+  if((inode.mode & INODE_DIR ) != INODE_DIR)
+    return -ENOTDIR;
+  
+  //verificar se o inode tem as permisoes de execução
+  if((stat = soAccessGranted(nInodeDir, X)) != 0)
+    return stat;
+  
+  //Verificar inconsistencia do directorio
+  if((stat = soQCheckDirCont(p_sb, &inode)) != 0)
+    return stat;
+  
+  int j;
+  int flag = -1;   
+ 
+  SODirEntry clusterDir[DPC]; 
+  
+  for(i = 0 ; i < (inode.size/(DPC*sizeof(SODirEntry))); i++){      
+      /* Função soReadFileCluster usada para ler um cluster que à partida é composto por directorios */
+      if ((stat=soReadFileCluster(nInodeDir, i, clusterDir)) != 0)    /* Read data cluster to ClsDir */
+         return stat;
+      
+       /* Se nome da entrada for igual ao nome recebido como parâmetro, então estamos no nó-i correcto.
+       * Retornar o valor do nó-i e o valor da próxima posição livre */
+      for(j = 0; j<DPC; j++){                                        /* Read every entry in data cluster */
+         if(strcmp(eName,(char*)(clusterDir[j].name)) == 0) {           /* If it finds an entry */
+         
+            if(p_nInodeEnt != NULL)
+               *p_nInodeEnt = (clusterDir[j].nInode); /* Point it to the requested value */
+            if(p_idx != NULL)
+               *p_idx = ((i*DPC)+j);                     /* Point it to idx if requested */
+               
+            return 0;
+         }
+         /* Encontrar a primeira posição LIVRE no estado LIMPO */
+      if ((clusterDir[j].name[0]=='\0') && (clusterDir[j].name[MAX_NAME]=='\0') && (flag == -1))
+      {
+        flag = i * DPC + j;
+      }
+    }
+  }
+
+  /* Se chegar aqui, significa que a entrada requerida não foi encontrada. Retornar o valor da próxima posição livre no estado LIMPO
+   * Se foi encontrada no cluster em questão alguma entrada nessas condições... */
+  if (flag != -1)
+  {
+    if (p_idx != NULL)
+      *p_idx = flag;
+  }
+  /* Senao, verificar se é possível alocar um novo cluster, e retornar a primeira posição dele */
+  else
+  {
+    if (i == MAX_FILE_CLUSTERS)
+      return -EFBIG;
+
+    if (p_idx != NULL)
+      *p_idx = i* DPC;
+  }
+
+  return -ENOENT;
 }
