6b7b3ce8e112eab5bdf3e776ff8a12557f0618dd
gjc <gjc@ua.pt>
2014-11-04 00:52:44 +0000

    makeresults.sh. upx. afinal os rst estavam todos erraodos. é necessario repetir todos os testes.

--- run/makeresults.sh
+++ run/makeresults.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+fusermount -u mnt
 rm val*rst
 
 for i in {1..8}
 do
  echo "A correr o ./val$i.sh"
- ./val1.sh > val$i.rst
+ ./val$i.sh > val$i.rst
  ./showblock_sofs14 -s 0 myDisk > val$i-sb.rst
 done
 
 cat val*rst > val_allresults.rst 
+fusermount -u mnt
