c8ced503f4e073c3985a4856c08f272289739d61
gjc <gjc@ua.pt>
2014-11-04 15:42:33 +0000

    makeresults.sh melhorias

--- run/makeresults.sh
+++ run/makeresults.sh
@@ -3,9 +3,17 @@
 fusermount -u mnt
 rm val*rst
 
+for i in {1..16}
+do
+ echo "A correr ./ex$i.sh"
+ ./ex$i.sh
+ ./showblock_sofs14 -s 0 myDisk > ex$i-sb.rst
+done
+
+
 for i in {1..8}
 do
- echo "A correr o ./val$i.sh"
+ echo -e "A correr o ./val$i.sh"
  ./val$i.sh > val$i.rst
  ./showblock_sofs14 -s 0 myDisk > val$i-sb.rst
 done
