af8f6b6d39b5fbaaecb29a47a52f9a240d2edc38
gjc@ua.pt <gjc@ua.pt@d350dffd-24a3-4275-a452-e23bb94b2ded>
2014-11-08 23:26:36 +0000

    melhorias

--- run/makeresults.sh
+++ run/makeresults.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-fusermount -u mnt
-rm val*rst
+# delete all old results
+rm *.rst
 
+# run all ./exX.sh and save its superblocks results to files
 for i in {1..16}
 do
  echo "A correr ./ex$i.sh"
@@ -10,6 +11,9 @@ do
  ./showblock_sofs14 -s 0 myDisk > ex$i-sb.rst
 done
 
+#merge all exX.sh results into one file
+cat testVector*rst > ex_allresults.rst
+cat ex*-sb.rst >> ex_allresults.rst
 
 for i in {1..8}
 do
@@ -18,5 +22,8 @@ do
  ./showblock_sofs14 -s 0 myDisk > val$i-sb.rst
 done
 
+# merge all valX.sh results in one file
 cat val*rst > val_allresults.rst 
+
+# make sure if something failed that mnt dir is unmounted
 fusermount -u mnt
